6fd8529cbd184bd494ef6074619974537f45046b
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-04 00:20:39 -0400

    Stage 1

--- anu_ex1.py
+++ anu_ex1.py
@@ -1,12 +1,20 @@
 print("Hi, I am new to python")
 print("hello again")
 print("What else can I learn here")
-
+print("i like typing this.")
+print("yay! I can print 'anything'")
+#trying single quotes outside and double inside
+print('anything "specific"')
+#worked
 a = 10
 b = 6
 c = 'philly '
 d = ' * New York * '
 print(c)
 print(c* a)
-print (d* 6)
-print (c+d)
+print(d* 6)
+print(c+d)
+
+# let's check the use of some comment #
+print("print this")
+#print("don't print this")

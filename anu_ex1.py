3854c5eec62fa14d2419447c876f576258f495b4
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-04 00:04:39 -0400

    Stage 1

---
+++ anu_ex1.py
@@ -0,0 +1,12 @@
+print("Hi, I am new to python")
+print("hello again")
+print("What else can I learn here")
+
+a = 10
+b = 6
+c = 'philly '
+d = ' * New York * '
+print(c)
+print(c* a)
+print (d* 6)
+print (c+d)
---
+++ anu_ex2.py
@@ -0,0 +1,15 @@
+#variables
+cars = 100
+spaceInCar = 4.0
+print(cars*spaceInCar)
+a,b,c = 1,3.0,"philly"
+print(a,b,c)
+d = "anu"
+print(c+d)
+print(c,d)
+print((a+b),c,d)
+print((a*b*spaceInCar)-cars)
+print(spaceInCar**b)
+print(b**spaceInCar)
+# starting new thing
+

792bda5314b95eb50d362398a0632f9ba4647463
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-09 00:23:43 -0400

    LearnPy hard way ex3

---
+++ anu_ex6plus.py
@@ -0,0 +1,37 @@
+print('I will count my pens')
+
+print('gel pen =', 10 + 5)
+print(' ball pen =', 10+6-3*2)
+
+print('Total pens are gel + ball pen =', 10+5+10+6-3*2)
+
+# lets do some algebraic equations
+
+a = 10+6*9+4-15+200/10
+print(int(a))
+# in algebraic equations, Python follows PEMDAS rule
+
+#lets do some more algebra
+
+print('what is 8+9 =', 8+9)
+
+print('is it true 5+7 is greater than 5-7?')
+print(5+7>5-7)
+print (5+7 < 5-7)
+
+l = 10
+m = 20
+n = 5
+print('Is l greater than m?', l>m)
+print(" Is m is greater than n?", m>n)
+print('Is l is less than or equal to m?', l<=m)
+print ('Is l is greater than or equal to n?', l>=n)
+
+# error or something that is don't know
+x = 20.55
+y = 20.91
+z = 20.50
+
+print((x + y))
+print(x - z)
+

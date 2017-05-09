2989707a06d22b346b109ff75087c35c5e3edc47
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-08 23:29:24 -0400

    Arithmetic and comparisons

--- anu_ex6.py
+++ anu_ex6.py
@@ -1,3 +1,42 @@
-# DIctionary data type
-s = {'name':'anu', 'age': 28, 'bday': 26.09}
-print(s['name'])
+# Arithmetic and Comparisons Operations
+a = 10
+b = 20
+
+#additon
+print(a+b)
+#Subtraction
+print(b-a)
+#multiplication
+print(a*b)
+#division
+print(b/a)
+#modulus
+print(b % a)
+#exponent
+c = 3
+print(a**c)
+
+# Comparisons
+
+x = 100
+y = 100
+z = 200
+w = 10
+
+#equal value of two operands
+
+if (x == y) :
+    print ( 'both x and y are equal')
+if (x ==z):
+    print ('x is not equal to z')
+# greater and less of two operands
+if (x < z):
+    print(' x is less than z')
+if (x > w):
+    print('x is greater than w')
+# greater than or equal to
+if (x>=w):
+    print('x is greater than or equal to w')
+if (x<=z):
+    print('x is less than or equal to z')
+

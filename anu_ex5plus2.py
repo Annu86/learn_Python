62def400c051c55632312075258f602f77b1b275
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-19 17:37:14 -0400

    Input ex11

---
+++ anu_ex5plus2.py
@@ -0,0 +1,11 @@
+# raw input ex 11
+
+print('How old are you?'),
+age = input()
+print('How tall are you?'),
+height = input()
+print('how much do you weigh'),
+weight = input()
+
+print('So, you are %r years old, %r feet tall and %r weigh' %(age, height, weight))
+

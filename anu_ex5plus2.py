5050a75926e6b7d9cfbc72e6ab08ee2b40583386
AnuBajpai <anuradhabajpai08@gmail.com>
2017-05-19 23:26:11 -0400

    Input ex11-12

--- anu_ex5plus2.py
+++ anu_ex5plus2.py
@@ -9,3 +9,16 @@ weight = input()
 
 print('So, you are %r years old, %r feet tall and %r weigh' %(age, height, weight))
 
+print('*'*30)
+
+color = input('what is you favorite color?') # removed comma after parenthesis closed and see the result
+shade = input('what is your favorite shade in that?')
+print(""" It means your favorite color is %r 
+and you favorite shade in that is %r""" %(color, shade))
+print('*'*30)
+
+# Lets do some more input
+cars = input('how many cars do you have?')
+
+print('I have %r cars.' %(cars), 'I am RICH :)')
+
